e0a2ba372ee7e860e773c08202a490383f8a1ee8
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-01-19 09:23:03 +0100

    ajout Tailwind

---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '../Recherche de joueur/recherche_de_joueur.html'
+  ],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
