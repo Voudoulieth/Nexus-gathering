870e7c555356b43998fc9f54e5774f3b30d8e7b9
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-02-07 19:25:30 +0100

    ajout feature Titre
    ajout d'une feature, changement dynamique du titre de la page recherche rapide résultat en fonction du jeux sélectionné dans la page recherche rapide

--- recherche_de_joueur/assets/js/recherche_rapide.js
+++ recherche_de_joueur/assets/js/recherche_rapide.js
@@ -1,8 +1,7 @@
-// recherche_rapide.js
-import { carouselData } from './data.js'; // Assurez-vous que le chemin vers data.js est correct
+import { carouselData } from './data.js';
 
 const carouselContent = document.getElementById("carousel-content"); // Conteneur pour le contenu dynamique
-let currentIndex = 0; // Commencez par le premier élément du tableau
+let currentIndex = 0; 
 
 function updateCarousel() {
     carouselContent.innerHTML = ''; // Efface le contenu précédent du carrousel
@@ -14,23 +13,22 @@ function updateCarousel() {
       // Détermine si l'élément actuel est le deuxième de la boucle
       let additionalAttributes = '';
       if (i === 2) { // Le deuxième élément a un index de 1 (0-based index)
-        additionalAttributes = ' id="imgspotlight"'; // Remplacez 'specialId' par l'identifiant que vous souhaitez ajouter
+        additionalAttributes = ' id="imgspotlight"';
       }
   
-      // Génère le contenu de l'élément avec l'id supplémentaire si nécessaire
-      const aTag = `<a href="${item.href}" class="carouselimg"${additionalAttributes}>
-        <figure>
-          <img src="${item.src}" alt="${item.alt}">
-          <figcaption>${item.caption}</figcaption>
-        </figure>
-      </a>`;
+      // Génère le contenu de l'élément
+      const aTag = `<a href="${item.href}" class="carouselimg"${additionalAttributes} data-caption="${encodeURIComponent(item.caption)}">
+      <figure>
+        <img src="${item.src}" alt="${item.alt}">
+        <figcaption>${item.caption}</figcaption>
+      </figure>
+    </a>`;
   
-      // Ajoutez le contenu généré au carrousel
+  
       carouselContent.innerHTML += aTag;
     }
   }
 
-// Ajoutez des écouteurs d'événements sur les boutons pour naviguer dans le carrousel
 document.getElementById('suivant').addEventListener('click', function() {
   currentIndex = (currentIndex + 1) % carouselData.length; // Incrémente l'index actuel
   updateCarousel();
@@ -41,5 +39,12 @@ document.getElementById('precedent').addEventListener('click', function() {
   updateCarousel();
 });
 
-// Initialisez le carrousel au chargement
+// Initialise le carrousel au chargement
 updateCarousel();
+
+document.querySelectorAll('.carouselimg').forEach(item => {
+  item.addEventListener('click', function(e) {
+    const caption = e.currentTarget.getAttribute('data-caption');
+    localStorage.setItem('selectedGameName', caption); // Utilisez sessionStorage si vous préférez
+  });
+});
