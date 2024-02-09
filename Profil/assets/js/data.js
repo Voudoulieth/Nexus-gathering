438ae0ef8bd9f45be3c29363654eea74286799ac
Lucian Laffont <lucianlaffont31@gmail.com>
2024-02-09 09:58:42 +0100

    maj lien + class JS

---
+++ Profil/assets/js/data.js
@@ -0,0 +1,21 @@
+/*  récuperer les données dans un tableau afin d'afficher que ceux ayant :checked
+    page ayant deux état : en mode édition ou non avec un boulean qui garde l'état de la page (edition oui/non)
+    récupérer les saisies
+    page mode non editable => affichage normal
+    page en mode editable => bouton radio etc 
+    Modifier la zone des photos afin de mettre des [+] et une fonction associé qui return console.log('ajoute un jeu') */
+
+class Profil {
+    #pseudo
+    #motdepass
+    #adressemail
+    #avatar
+    #tempsdejeux
+    #niveaudejeux
+    #typedejeux //tableaux
+    #bio // zone de texte
+    #interets // zone de texte
+    #modedejeux
+    n
+    
+}
