f3412e303eee36149fe399141848acb807fb3bb2
Corentin Malecki <Corentin.Malecki@gmail.com>
2024-02-11 12:04:59 +0100

    JS a foison

---
+++ bibliotheques/JS/script_biblio_generale.js
@@ -0,0 +1,15 @@
+import * as data from './data.js';
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Afficher la liste des jeux
+    function afficherListeJeu() {
+        let listejeu = document.querySelector('#listejeu');
+        listejeu.innerHTML = "";
+        data.jeu.forEach(function(jeu){
+            let jeuElement = document.createElement("div");
+            jeuElement.innerHTML = jeu.getJeu();
+            listejeu.appendChild(jeuElement);
+        });
+    }
+    afficherListeJeu();
+});
