d9523d212428a4feac38071545d3893e3074e40f
Lucian Laffont <lucianlaffont31@gmail.com>
2024-02-13 11:12:56 +0100

    Modification JS
    Création classe Profil
    modification edition / sauvegarde / annulation

--- Profil/assets/js/data.js
+++ Profil/assets/js/data.js
@@ -7,15 +7,50 @@
 
 class Profil {
     #pseudo
-    #motdepass
-    #adressemail
+    #password
+    #email
     #avatar
-    #tempsdejeux
-    #niveaudejeux
-    #typedejeux //tableaux
+    #temps
+    #niveau
+    #type //tableaux
     #bio // zone de texte
     #interets // zone de texte
-    #modedejeux
-    n
+    #mode
+
+    constructor(pseudo,password,email,avatar,temps,niveau,type,bio,interets,mode) {
+        this.#pseudo    = pseudo;
+        this.#password  = password;
+        this.#email     = email;
+        this.#avatar    = avatar;
+        this.#temps     = temps;
+        this.#niveau    = niveau;
+        this.#type      = type;
+        this.#bio       = bio;
+        this.#interets  = interets;
+        this.#mode      = mode;
+
+    }
+    
+    get pseudo()    {return this.#pseudo}
+    get password()  {return this.#password}
+    get email()     {return this.#email}
+    get avatar()    {return this.#avatar}
+    get temps()     {return this.#temps}
+    get niveau()    {return this.#niveau}
+    get type()      {return this.#type}
+    get bio()       {return this.#bio}
+    get interets()  {return this.#interets}
+    get mode()      {return this.#mode}
+
+
+    set password(password) {this. #password = password}
+    set email(email) {this.#email = email}
+    set avatar(avatar) {this.#avatar = avatar}
+    set temps(temps) {this.#temps = temps}
+    set niveau(niveau) {this.#niveau = niveau}
+    set type(type) {this.#type = type}
+    set bio(bio) {this.#bio = bio}
+    set interets(interets) {this.#interets = interets}
+    set mode(mode) {this.#mode = mode}
     
 }
