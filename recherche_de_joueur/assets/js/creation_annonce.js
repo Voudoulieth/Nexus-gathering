b0a80adabb94aebaea40d718dc037e6176470652
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-02-13 16:46:05 +0100

    ajout de css
    ajustement de l'affichage du texte descriptif de l'annonce
    ajout d'un style pour scrollbar

--- recherche_de_joueur/assets/js/creation_annonce.js
+++ recherche_de_joueur/assets/js/creation_annonce.js
@@ -4,14 +4,16 @@ function toggleDisplay(showAnnonce) {
     const annonceDisplay = document.getElementById('annonceDisplay');
 
     if (showAnnonce) {
+        document.getElementById('h1').textContent = "Votre annonce"
         form.style.display = 'none';
-        annonceDisplay.style.display = 'block';
+        annonceDisplay.style.display = 'flex';
         // Remplir les détails de l'annonce ici
         document.getElementById('annonceTitle').textContent = document.querySelector('.formNom').value;
         document.getElementById('annonceGameName').textContent = document.getElementById('nom_du_jeu').value;
         document.getElementById('annoncePlayerCount').textContent = document.getElementById('nbjoueur').value;
         document.getElementById('annonceDescription').textContent = document.getElementById('textDesc').value;
     } else {
+        document.getElementById('h1').textContent = "Création de l'annonce"
         form.style.display = 'flex';
         annonceDisplay.style.display = 'none';
     }
@@ -43,29 +45,31 @@ document.getElementById('textDesc').addEventListener('input', togglePlaceholderD
 
 const inputNumber = document.getElementById("nbjoueur");
 
-// Fonction pour mettre à jour la valeur de l'input
-function updateValue(isIncrementing) {
-    let currentValue = parseInt(inputNumber.value) || 2; // Obtient la valeur actuelle de l'input, ou 0 si non numérique
-    if (isIncrementing) {
-        currentValue = currentValue < 64 ? currentValue + 1 : 64; // Incrémente si moins de 64
+// Fonction pour mettre à jour la valeur de l'input en respectant les limites
+const updateValue = (isIncrementing) => {
+    let currentValue = parseInt(inputNumber.value) || 2; // Obtient la valeur actuelle de l'input, ou 2 si non numérique
+    if (isIncrementing !== undefined) {
+        // Incrément ou décrément basé sur le bouton cliqué
+        currentValue = isIncrementing ? Math.min(currentValue + 1, 64) : Math.max(currentValue - 1, 2);
     } else {
-        currentValue = currentValue > 2 ? currentValue - 1 : 2; // Décrémente si plus de 2
+        // Ajuste la valeur si saisie manuellement hors limites
+        currentValue = Math.min(Math.max(currentValue, 2), 64);
     }
     inputNumber.value = currentValue; // Met à jour la valeur de l'input
-}
+};
 
 // Fonctions enveloppantes pour incrémenter et décrémenter
-function incrementValue() {
-    updateValue(true);
-}
-
-function decrementValue() {
-    updateValue(false);
-}
+const incrementValue = () => updateValue(true);
+const decrementValue = () => updateValue(false);
 
 // Ajoute des écouteurs d'événements pour les boutons + et -
 document.querySelector('.control-up').addEventListener('click', incrementValue);
 document.querySelector('.control-down').addEventListener('click', decrementValue);
+
+// Ajoute un écouteur d'événements pour valider la saisie manuelle
+inputNumber.addEventListener('input', () => updateValue());
+
+
 
 function resetForm() {
     // Réinitialiser les valeurs des champs
@@ -78,3 +82,17 @@ function resetForm() {
     document.getElementById('placeholderText').style.display = 'block';
 }
 document.getElementById('deleteAnnonce').addEventListener('click', resetForm);
+
+const validateInput = (inputElement) => {
+    const value = inputElement.value;
+    if (/<[^>]+>/i.test(value)) {
+        alert('La saisie de code HTML n\'est pas autorisée.');
+        inputElement.value = value.replace(/<[^>]+>/gm, '');
+    }
+    
+};
+document.getElementById('textDesc').addEventListener('input', () => validateInput(document.getElementById('textDesc')));
+document.querySelectorAll('.formNom').forEach((inputElement) => {
+    inputElement.addEventListener('input', () => validateInput(inputElement));
+});
+
