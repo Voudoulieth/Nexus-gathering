e0703803ccacab4b63adc5b372e6b132ef55d316
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-02-13 01:15:24 +0100

    rejoindre une annonce
    ajout d'une nouvelle page pour affiché les annonces

---
+++ recherche_de_joueur/assets/js/rejoindre_une_annonce.js
@@ -0,0 +1,25 @@
+const inputNumber = document.getElementById("nbjoueur");
+
+// Fonction pour mettre à jour la valeur de l'input
+function updateValue(isIncrementing) {
+    let currentValue = parseInt(inputNumber.value) || 2; // Obtient la valeur actuelle de l'input, ou 0 si non numérique
+    if (isIncrementing) {
+        currentValue = currentValue < 64 ? currentValue + 1 : 64; // Incrémente si moins de 64
+    } else {
+        currentValue = currentValue > 2 ? currentValue - 1 : 2; // Décrémente si plus de 2
+    }
+    inputNumber.value = currentValue; // Met à jour la valeur de l'input
+}
+
+// Fonctions enveloppantes pour incrémenter et décrémenter
+function incrementValue() {
+    updateValue(true);
+}
+
+function decrementValue() {
+    updateValue(false);
+}
+
+// Ajoute des écouteurs d'événements pour les boutons + et -
+document.querySelector('.control-up').addEventListener('click', incrementValue);
+document.querySelector('.control-down').addEventListener('click', decrementValue);
