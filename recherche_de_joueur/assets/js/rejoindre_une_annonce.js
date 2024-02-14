61682f39fae2d323d14f12ae14b73ed434136953
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-02-14 01:22:22 +0100

    petite modifcation
    changement de détail visuel des annonces et préparation pour l'ajout et le filtrage des annonces

--- recherche_de_joueur/assets/js/rejoindre_une_annonce.js
+++ recherche_de_joueur/assets/js/rejoindre_une_annonce.js
@@ -1,25 +1,25 @@
 const inputNumber = document.getElementById("nbjoueur");
 
-// Fonction pour mettre à jour la valeur de l'input
-function updateValue(isIncrementing) {
-    let currentValue = parseInt(inputNumber.value) || 2; // Obtient la valeur actuelle de l'input, ou 0 si non numérique
-    if (isIncrementing) {
-        currentValue = currentValue < 64 ? currentValue + 1 : 64; // Incrémente si moins de 64
+// Fonction pour mettre à jour la valeur de l'input en respectant les limites
+const updateValue = (isIncrementing) => {
+    let currentValue = parseInt(inputNumber.value) || 1; // Obtient la valeur actuelle de l'input, ou 1 si non numérique
+    if (isIncrementing !== undefined) {
+        // Incrément ou décrément basé sur le bouton cliqué
+        currentValue = isIncrementing ? Math.min(currentValue + 1, 63) : Math.max(currentValue - 1, 1);
     } else {
-        currentValue = currentValue > 2 ? currentValue - 1 : 2; // Décrémente si plus de 2
+        // Ajuste la valeur si saisie manuellement hors limites
+        currentValue = Math.min(Math.max(currentValue, 1), 63);
     }
     inputNumber.value = currentValue; // Met à jour la valeur de l'input
-}
+};
 
 // Fonctions enveloppantes pour incrémenter et décrémenter
-function incrementValue() {
-    updateValue(true);
-}
-
-function decrementValue() {
-    updateValue(false);
-}
+const incrementValue = () => updateValue(true);
+const decrementValue = () => updateValue(false);
 
 // Ajoute des écouteurs d'événements pour les boutons + et -
 document.querySelector('.control-up').addEventListener('click', incrementValue);
 document.querySelector('.control-down').addEventListener('click', decrementValue);
+
+// Ajoute un écouteur d'événements pour valider la saisie manuelle
+inputNumber.addEventListener('input', () => updateValue());
