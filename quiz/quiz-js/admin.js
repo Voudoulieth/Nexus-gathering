12e7d8bd7357bba9e388d834c808e74890954a30
oceane faurie <faurie.oce@gmail.com>
2024-02-17 17:16:38 +0100

    Mise au propre du code - quiz

--- quiz/quiz-js/admin.js
+++ quiz/quiz-js/admin.js
@@ -1,51 +1,45 @@
 import * as data from './data.js';
 
-let quizAdmin = [...data.communautaires,...data.officiels];
-let galerie = document.querySelector("#galerieAdmin");
-// Affichage de tous les quiz existants
+const quizAdmin = [...data.communautaires,...data.officiels];
+const galerie   = document.querySelector("#galerieAdmin");
 
+// ETAPE 1 : Affichage de tous les quiz existants, affichage par défaut à l'arrivée sur la page
 quizAdmin.forEach(quiz => galerie.innerHTML += quizHTML(quiz));
 
 function quizHTML(quiz){
-    return `<figure id="${quiz.id}">
-    <figcaption>
-        <p>${quiz.titre} - créé par ${quiz.auteur} - </p>
-        <time datetime="${quiz.date}">${quiz.date}</time>
-    </figcaption>
-    <a href="${quiz.lien}">
-        <img src="${quiz.img}">
-    </a>
+    let type = (quiz.type === "communautaire") ? ` - créé par ${quiz.auteur}` : "";
+    return `
+    <figure id="${quiz.id}">
+        <figcaption>
+            <p>${quiz.titre}${type}</p>
+            <time datetime="${quiz.date}">${quiz.date}</time>
+        </figcaption>
+        <a href="${quiz.lien}">
+            <img src="${quiz.img}" alt="${quiz.titre}">
+        </a>
     </figure>`;
 }
 
-// Gestion de l'affichage des quiz en fonction du radio sélectionné
+// ETAPE 2 : Gestion de l'affichage des quiz en fonction de l'input de type radio sélectionné
+const panneauAdmin  = document.querySelector("#panneauAdmin");
+const radios        = panneauAdmin.querySelectorAll("input"); // Récupère tous les <input> dans la <div> identifiée #panneauAdmin
 
-let panneauAdmin=document.querySelector("#panneauAdmin");
-
-// listener sur les <input> des types de quiz
-let radios = panneauAdmin.querySelectorAll("input");
 radios.forEach(radio => radio.addEventListener("click", typeQuiz));
 
-// recupere l'element html (le <input>) sur lequel l'utilisateur a click dans le menu (type de plat)
 function typeQuiz(event) {
-    let typeRadio = event.target; //target l'input cliqué
-    let quizChoisis = recupQuizChoisis(typeRadio.id); // Les id possibles sont : communautaire, officiel, tous
-    galerie.innerHTML=''; // Vider la galerie afin de pouvoir mettre la sélection de quiz choisis
-
+    const typeRadio   = event.target; // Cible le <input> cliqué
+    const quizChoisis = recupQuizChoisis(typeRadio.id); // Lance la fonction qui va récupérer les quiz selon leur type
+    galerie.innerHTML = ''; // Vider la galerie afin de pouvoir mettre la sélection de quiz choisis
     quizChoisis.forEach(quiz => galerie.innerHTML += quizHTML(quiz));
 }
 
-// recupere les quiz qui ont pour type ${id}
-function recupQuizChoisis(id) {
-
-    let tableauQuizChoisis = new Array();
-    
-        if (id==="tous") tableauQuizChoisis=quizAdmin;
-        else {
-            quizAdmin.forEach(quiz=>{
-                if (id===quiz.type) tableauQuizChoisis.push(quiz);
-            });
-        };
-    console.log(tableauQuizChoisis);
+function recupQuizChoisis(id) { // Les id possibles sont : communautaire, officiel, tous (ce sont les id des différents <input>, ainsi l'id en paramètre de la fonction dépend de l'<input> cliqué)
+    let tableauQuizChoisis = []; // Le tableau dans lequel on mettra les quiz correspondant au type voulu
+    if (id === "tous") tableauQuizChoisis = quizAdmin;
+    else {
+        quizAdmin.forEach(quiz => {
+            if (id === quiz.type) tableauQuizChoisis.push(quiz);
+        });
+    };
     return tableauQuizChoisis;
 }
--- quiz/quiz-js/officiels.js
+++ quiz/quiz-js/officiels.js
@@ -1,15 +1,16 @@
 import * as data from './data.js';
 
+const galerie = document.querySelector("#galerieOfficiel");
+
 data.officiels.forEach(quiz => {
-    let galerie = document.querySelector("#galerieOfficiel");
-    galerie.innerHTML +=
-    `<figure id="${quiz.id}">
-    <figcaption>
-        <p>${quiz.titre} - créé par ${quiz.auteur} - </p>
-        <time datetime="${quiz.date}">${quiz.date}</time>
-    </figcaption>
-    <a href="${quiz.lien}">
-        <img src="${quiz.img}">
-    </a>
+    galerie.innerHTML += `
+    <figure id="${quiz.id}">
+        <figcaption>
+            <p>${quiz.titre} - </p>
+            <time datetime="${quiz.date}">${quiz.date}</time>
+        </figcaption>
+        <a href="${quiz.lien}">
+            <img src="${quiz.img}" alt="${quiz.titre}">
+        </a>
     </figure>`;
 });
