fd3f3a059ae39c92b6e8bc66a856ed5a0f3f5a7b
Corentin Malecki <Corentin.Malecki@gmail.com>
2024-04-09 14:01:39 +0200

    preparation du head

---
+++ src/view/head.inc.php
@@ -0,0 +1,19 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta
+    name="description"
+    content="Retrouvez la bibliothèque générale ou privée ici!"
+    />
+    <!-- Import Changa -->
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+    href="https://fonts.googleapis.com/css2?family=Changa&display=swap"
+    rel="stylesheet"
+    />
+    <!-- Feuilles de style CSS -->
+    <link rel="stylesheet" href="../css/reset.css" />
+    <link rel="stylesheet" href="../css/style_global.css" />
+    <link rel="shortcut icon" href="../assets/logo/favicon.svg" />
+</head>
