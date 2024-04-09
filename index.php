022cc50cbac393cd6fc85f87d7eef29ba092b003
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-04-09 15:20:53 +0200

    changement index

--- index.php
+++ index.php
@@ -1,12 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
   <head>
-    <?php include '../src/view/head.inc.php' ?>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Rejoignez une communauté de joueurs, trouvez vos coéquipiers et maintenez a jours vos collections!">
+    <!-- Import Changa -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Changa&display=swap" rel="stylesheet">
+    <!-- Feuilles de style CSS -->
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/style_global.css">
     <link rel="stylesheet" href="./css/style_index.css">
+    <!-- <script defer src=""></script> -->
+    <link rel="shortcut icon" href="./assets/logo/favicon.svg">
     <title>Accueil - Nexus Gathering</title>
   </head>
   <body>
-  <?php include '../src/view/header.inc.php' ?>
+    <header>
+        <a href="./index.php">
+            <img class="logo" src="./assets/logo/logo_nexus_white.svg" title="Accueil" alt="Logo Nexus">
+        </a>
+        <div id="menuburger">
+            <nav>
+                <ul>
+                    <li><a class="navlink" href="./bibliotheques/vacceuilbiblio.php">Bibliothèque</a></li>
+                    <li><a class="navlink" href="./recherche_de_joueur/recherche_de_joueurs.php">Joueurs</a></li>
+                    <li><a class="navlink" href="./quiz/quiz-accueil.php">Quiz</a></li>
+                    <li><a class="navlink" href="./recherche_de_joueur/messagerie.php">Messagerie</a></li>
+                </ul>
+            </nav>
+            <div class="header-moitie">
+                <div class="rechercher">
+                    <input class="FormulaireRechercher navlink" type="text" placeholder="Rechercher">
+                    <button class="SubmitRecherche" type="submit"><img src="./assets/Icone/magnifying-glass-solid-blanc.svg"></button>
+                </div>
+                <button class="BoutonConnexion navlink">Connexion</button>
+            </div>
+        </div>
+        <a href="#" id="openMenuBurger"><!--TODO javascript pour ouvrir le menu burger et afficher la navbar mobile-->
+            <span class="burger-icon">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+        </a>
+    </header>
     <main>
       <div id="titlecarousel">
         <h1 class="titres">NOUVEAUTES</h1>
@@ -32,7 +71,22 @@
         </a>
       </div>
     </main>
-    <?php include '../src/view/footer.inc.php' ?>
+    <footer>
+      <div>
+        <a href="./index.php">
+          <img class="logo" src="./assets/logo/logo_long_nexus_white.svg" title="Acceuil" alt="Logo Nexus Blanc">
+        </a>
+        <nav>
+          <ul>
+            <li><a class="navlink" href="./cgu.php">CGU</a></li>
+            <li><a class="navlink" href="./mention_legale.php">Mention légale</a></li>
+            <li><a class="navlink" href="./confidentialite.php">Politique de confidentialité</a></li>
+            <li><a class="navlink" href="./contact.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <p class="copyright">COPYRIGHT @ 2024 TOUS DROITS RESERVE</p>
+    </footer>
     <script type="module" src="./js/index.js"></script>
   </body>
 </html>
