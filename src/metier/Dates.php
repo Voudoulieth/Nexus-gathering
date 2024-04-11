0e8cadb812f1b0dc0d011faaea8d9dc764717619
Corentin Malecki <Corentin.Malecki@gmail.com>
2024-04-11 10:30:21 +0200

    modif date class Dates

--- src/metier/Dates.php
+++ src/metier/Dates.php
@@ -3,29 +3,29 @@ declare(strict_types=1);
 // namespace restoV5b\metier;
 
 class Dates {
-    private date     $ddate;
-    private time     $hdate;
+    private datetime     $ddate;
+    private string       $hdate;
 
     //TODO VOIR DATE ET TIME EN PHP
 
     public function __construct($ddate, $hdate) {
-        $this->ddate       = $ddate;
+        $this->ddate  = $ddate;
         $this->hdate  = $hdate;
     }
 
-    public function getDdate(): date {
+    public function getDdate(): datetime {
         return $this->ddate;
     }
-    public function setDdate(date $ddate) {
+    public function setDdate(datetime $ddate) {
         $this->ddate = $ddate;
     }
-    public function getHdate(): time {
+    public function getHdate(): string {
         return $this->hdate;
     }
-    public function setHdate(time $hdate) {
+    public function setHdate(string $hdate) {
         $this->hdate = $hdate;
     }
-    public function __toString() {
-        return '[Categorie : '.$this->ddate . ',' . $this->hdate .']';
+    public function __toString(): string {
+        return '[Date : '.$this->ddate->format('Y-m-d') . ', Time : ' . $this->hdate . ']';
     }
 }
