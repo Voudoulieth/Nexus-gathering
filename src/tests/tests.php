dc3e51a91e65db7db6b7767b364f49dd48bf1d46
Lucian Laffont <lucianlaffont31@gmail.com>
2024-04-21 22:30:33 +0200

    test

--- src/tests/tests.php
+++ src/tests/tests.php
@@ -3,11 +3,14 @@ require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
 
 use Nexus_gathering\dao\DaoNexus;
 use Nexus_gathering\metier\Jeu;
+use Nexus_gathering\metier\RoleUtilisateur;
 
-$jeuDAO = new JeuDAO($pdo);
+$jeuDAO = new RoleUtilisateur(1,"admin");
+
+var_dump($jeuDAO);
 
 // Appel de la fonction getJeux() pour obtenir les jeux
-$jeux = $jeuDAO->getJeux();
+// $jeux = $jeuDAO->getJeux();
 
 // Affichage des jeux récupérés (à des fins de débogage)
-var_dump($jeux);
+// var_dump($jeux);
