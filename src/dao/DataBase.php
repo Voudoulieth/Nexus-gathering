629c611bc157f4bf2a8905e12ae6535dddab1a88
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-04-21 22:19:02 +0200

    test

--- src/dao/DataBase.php
+++ src/dao/DataBase.php
@@ -11,13 +11,12 @@ class Database {
     private static \PDO $db;
 
     public static function getConnection() : \PDO {
-        if (!isset(self::$db)) { // s'il y a erreur, cela sera catch par DAOResto.php
+        if (!isset(self::$db)) {
             if (file_exists("../param.ini")) {
                 $param = parse_ini_file("../param.ini", true);
-                extract($param['BDD']);     // extract du tag [BDD] et génère les variables $host, $port ... du nom donne dans param.ini          
+                extract($param['BDD']);       
             } 
             else throw new DaoException("Parametre BDD indisponibles",8001);
-            // On cree le data source name 'mysql:host=127.0.0.1:3306;dbname=testdomi;charset=utf8'
             $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8";
             self::$db = new \PDO($dsn, $user, $password);
             self::$db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
