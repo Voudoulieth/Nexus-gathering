e6142f25c298762a76aac4c9ff4db2cd62b71503
Lucas Legendre <lucas.lucasleg@gmail.com>
2024-04-22 10:48:57 +0200

    maj jeu d'essai

--- src/tests/tests.php
+++ src/tests/tests.php
@@ -21,15 +21,8 @@ $dateMessage = new DateTime(); // Initialise avec la date et l'heure actuelle
 // Création d'un objet Messages avec tous les paramètres requis
 $message = new Messages($messageId, $contenu, $modif, $idExped, $idDesti, $dateMessage);
 
-// // Appeler la fonction createMessage
-// $result = $dao->createMessage($message);
-
-// // Afficher le résultat
-// if ($result) {
-//     echo "Le message a été créé avec succès.";
-// } else {
-//     echo "Erreur lors de la création du message.";
-// }
+// Appeler la fonction createMessage
+$result = $dao->createMessage($message);
 
 // Afficher le résultat
 if ($result) {
@@ -38,38 +31,30 @@ if ($result) {
     echo "Erreur lors de la création du message.";
 }
 
-// // Test getConversationMessages
-// $conversationMessages = $dao->getConversationMessages($idExped, $idDesti);
-// echo "Conversation entre $idExped et $idDesti:\n";
-// print_r($conversationMessages);
-
 // Test getConversationMessages
 $conversationMessages = $dao->getConversationMessages($idExped, $idDesti);
 echo "Conversation entre $idExped et $idDesti:\n";
 print_r($conversationMessages);
 
-// // Appeler la fonction getUserConversations
-// $conversations = $dao->getUserConversations($id_user);
 
-// // Afficher les résultats
-// echo "Conversations pour l'utilisateur $id_user:\n";
-// foreach ($conversations as $conversation) {
-//     echo "ID Utilisateur: " . $conversation['id_user'] . ", Nom: " . $conversation['nom_user'] . ", Dernier Message: " . $conversation['last_message_date'] . "\n <br><br>";
-// }
+// Appeler la fonction getUserConversations
+$conversations = $dao->getUserConversations($id_user);
+
 // Afficher les résultats
 echo "Conversations pour l'utilisateur $id_user:\n";
 foreach ($conversations as $conversation) {
-    echo "ID Utilisateur: " . $conversation['id_user'] . ", Nom: " . $conversation['nom_user'] . ", Dernier Message: " . $conversation['last_message_date'] . "\n<br><br>";
+    echo "ID Utilisateur: " . $conversation['id_user'] . ", Nom: " . $conversation['nom_user'] . ", Dernier Message: " . $conversation['last_message_date'] . "\n <br><br>";
 }
 
-// // Test updateMessage
-// $nouveauContenu = "Ceci est le contenu modifié.";
-// $updateStatus = $dao->updateMessage($messageId, $nouveauContenu);
-// echo $updateStatus ? "Le message $messageId a été mis à jour.\n" : "Échec de la mise à jour du message $messageId.\n";
 
-// // Test deleteMessage
-// $deleteStatus = $dao->deleteMessage($messageId);
-// echo $deleteStatus ? "Le message 6 a été supprimé.\n" : "Échec de la suppression du message $messageId.\n";
+// Test updateMessage
+$nouveauContenu = "Ceci est le contenu modifié.";
+$updateStatus = $dao->updateMessage($messageId, $nouveauContenu);
+echo $updateStatus ? "Le message $messageId a été mis à jour.\n" : "Échec de la mise à jour du message $messageId.\n";
+
+// Test deleteMessage
+$deleteStatus = $dao->deleteMessage($messageId);
+echo $deleteStatus ? "Le message 6 a été supprimé.\n" : "Échec de la suppression du message $messageId.\n";
 
 
 // Définir les données de test pour un jeu
