d2914aa44b7286ac0b3f5884769c5a80f66e91c7
Corentin Malecki <Corentin.Malecki@gmail.com>
2024-08-08 14:07:06 +0200

    maj

--- src/view/success.php
+++ src/view/success.php
@@ -9,7 +9,7 @@
         <div class="text-center pt-5">
             <h1 class="font-['Changa'] text-[4.5em] p-5">Opération réussie</h1>
             <p class="text-[0.875em]">
-                Le jeu a été ajouté avec succès.
+                Votre demande a été effectué avec succès.
             </p>
             <a href="<?= APP_ROOT ?>/ajout-biblio-generale">Ajouter un autre jeu</a>
         </div>
